ea141f6d942e962d9a7890c509d360cfab4f6642
Vincent Gu <g@v-io.co>
2017-04-13 19:55:46 +0800

    update variables and configs

--- Dockerfile
+++ Dockerfile
@@ -2,39 +2,21 @@
 # Dockerfile for kcptun-server
 #
 
-FROM alpine
+FROM lisnaz/kcptun:latest
 MAINTAINER Vincent.Gu <g@v-io.co>
 
-ENV KCPTUN_LISTEN_ADDR   ""
-ENV KCPTUN_LISTEN_PORT   29000
-ENV KCPTUN_TARGET_ADDR   ""
-ENV KCPTUN_TARGET_PORT   8388
-ENV KCPTUN_KEY           password
-ENV KCPTUN_CRYPT         aes
-ENV KCPTUN_MODE          fast2
-ENV KCPTUN_CONN          1
-ENV KCPTUN_AUTO_EXPIRE   0
-ENV KCPTUN_MTU           1200
-ENV KCPTUN_SNDWND        1024
-ENV KCPTUN_RCVWND        1024
-ENV KCPTUN_DATASHARD     10
-ENV KCPTUN_PARITYSHARD   3
-ENV KCPTUN_DSCP          46
-ENV KCPTUN_NOCOMP        true
-ENV KCPTUN_LOG           /dev/null
+ENV KCPTUN_SERVER_LISTEN_ADDR   127.0.0.1
+ENV KCPTUN_SERVER_LISTEN_PORT   8388
+ENV KCPTUN_SERVER_TARGET_ADDR   127.0.0.1
+ENV KCPTUN_SERVER_TARGET_PORT   4000
 
-# define default directory
-ENV APP_DIR              /srv/kcptun
-WORKDIR $APP_DIR
-
-EXPOSE $KCPTUN_LISTEN_PORT/tcp
+EXPOSE $KCPTUN_SERVER_LISTEN_PORT/tcp
 
 ADD entrypoint.sh /
 ENTRYPOINT ["/entrypoint.sh"]
 
-# build kcptun
-ENV KCPTUN_VER 20170329
-ENV KCPTUN_URL https://github.com/xtaci/kcptun/releases/download/v${KCPTUN_VER}/kcptun-linux-amd64-${KCPTUN_VER}.tar.gz
+# install dependencies
+ENV KCPTUN_DEP bash
 RUN set -ex \
-    && curl -sSL $KCPTUN_URL | tar xz \
-    && rm -rf /var/cache/apk/*
+    && apk add --update --no-cache $KCPTUN_DEP \
+    && rm -rf /var/cache/apk/* \
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,31 +1,15 @@
 #!/usr/bin/env bash
-
 set -e
 
+# ====== Internal Variables ======
 KCPTUN_CONF="server-config.json"
-# ======= KCPTUN CONFIG ======
-KCPTUN_LISTEN_ADDR=${KCPTUN_LISTEN_ADDR:-0.0.0.0}             #"listen addr": "0.0.0.0"
-KCPTUN_LISTEN_PORT=${KCPTUN_LISTEN_PORT:-29900}               #"listen addr": "29900"
-KCPTUN_TARGET_ADDR=${KCPTUN_TARGET_ADDR:-127.0.0.1}           #"target addr": "127.0.0.1"
-KCPTUN_TARGET_PORT=${KCPTUN_TARGET_PORT:-8388}                #"target port": "8388"
-KCPTUN_KEY=${KCPTUN_KEY:-password}                            #"key": "password",
-KCPTUN_CRYPT=${KCPTUN_CRYPT:-aes}                             #"crypt": "aes",
-KCPTUN_MODE=${KCPTUN_MODE:-fast2}                             #"mode": "fast2",
-KCPTUN_CONN=${KCPTUN_CONN:-1}                                 #"conn": 1,
-KCPTUN_AUTO_EXPIRE=${KCPTUN_AUTO_EXPIRE:0}                    #"autoexpire": 0,
-KCPTUN_MTU=${KCPTUN_MTU:-1350}                                #"mtu": 1350,
-KCPTUN_SNDWND=${KCPTUN_SNDWND:-1024}                          #"sndwnd": 1024,
-KCPTUN_RCVWND=${KCPTUN_RCVWND:-1024}                          #"rcvwnd": 1024,
-KCPTUN_DATASHARD=${KCPTUN_DATASHARD:-10}                      #"datashard": 10,
-KCPTUN_PARITYSHARD=${KCPTUN_PARITYSHARD:-10}                  #"parityshard": 3,
-KCPTUN_DSCP=${KCPTUN_DSCP:-46}                                #"dscp": 46
-KCPTUN_NOCOMP=${KCPTUN_NOCOMP:-true}                          #"nocomp": true
-KCPTUN_LOG=${KCPTUN_LOG:-/dev/null}                           #"log": /dev/null
+# ====== Internal Variables ======
 
+# ====== Generate KCPTUN config ======
 cat > ${KCPTUN_CONF}<<-EOF
 {
-    "listen": "${KCPTUN_LISTEN_ADDR}:${KCPTUN_LISTEN_PORT}",
-    "target": "${KCPTUN_TARGET_ADDR}:${KCPTUN_TARGET_PORT}",
+    "listen": "${KCPTUN_SERVER_LISTEN_ADDR}:${KCPTUN_SERVER_LISTEN_PORT}",
+    "target": "${KCPTUN_SERVER_TARGET_ADDR}:${KCPTUN_SERVER_TARGET_PORT}",
     "key": "${KCPTUN_KEY}",
     "crypt": "${KCPTUN_CRYPT}",
     "mode": "${KCPTUN_MODE}",
@@ -41,11 +25,13 @@ cat > ${KCPTUN_CONF}<<-EOF
     "log": "${KCPTUN_LOG}"
 }
 EOF
+# ====== Generate KCPTUN config ======
 
-echo "Starting Kcptun for Shadowsocks-libev..."
-./client_linux_amd64 -v
+
+echo "Starting Kcptun server ..."
+./server_linux_amd64 -v
 echo "+---------------------------------------------------------+"
 echo "KCP Listen     : ${KCPTUN_LISTEN_ADDR}:${KCPTUN_LISTEN_PORT}"
-echo "KCP Parameter: --crypt ${KCPTUN_CRYPT} --key ${KCPTUN_KEY} --mtu ${KCPTUN_MTU} --sndwnd ${KCPTUN_SNDWND} --rcvwnd ${KCPTUN_RCVWND} --mode ${KCPTUN_MODE} --nocomp ${KCPTUN_NOCOMP}" --dscp ${KCPTUN_DSCP}
+echo "KCP Parameter: --crypt ${KCPTUN_CRYPT} --key ${KCPTUN_KEY} --mtu ${KCPTUN_MTU} --sndwnd ${KCPTUN_SNDWND} --rcvwnd ${KCPTUN_RCVWND} --mode ${KCPTUN_MODE} --nocomp ${KCPTUN_NOCOMP} --dscp ${KCPTUN_DSCP}"
 echo "+---------------------------------------------------------+"
-exec "./client_linux_amd64" -c ${KCPTUN_CONF}
+exec "./server_linux_amd64" -c ${KCPTUN_CONF}
